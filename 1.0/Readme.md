86873592a2d462b076b72b7f5aa61786809f06d2
Eyad-Syrialover <eyadbere@gmail.com>
2016-08-16 12:48:48 +0300

    Add files via upload

--- 1.0/Readme.md
+++ 1.0/Readme.md
@@ -1,1 +1,8 @@
-
+<h1>Version 1.0 Released On 16/8/2016</h1>
+<h2>What's New On Version 1.0?</h2>
+<h3>
+<ul>
+<li>This Version Of sy-01.js Is The First On The Road.</li>
+<li>With This Version You Can Encode And Decode The Letters In English Alphabet.</li>
+</ul>
+</h3>
---
+++ 1.0/sy-01.js
@@ -0,0 +1,261 @@
+/*
+SY-01.JS V1.0 CREATED BY EYAD SYRIA-LOVER ON 15/8/2016.
+
+LICENSED UNDER THE TERMS OF WTFPL LICENSE
+
+
+   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+window.onload=function(){
+var i;
+for (i=0;i<document.querySelectorAll("[data-sy-01=true]").length;i++)
+{
+/*********************************  Encoding Lower Case
+*********************************/
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/a/g,"1000000000000000000000000002"); 
+
+document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/b/g,"0100000000000000000000000002"); 
+
+document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/c/g,"0010000000000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/d/g,"0001000000000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/e/g,"0000100000000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/f/g,"0000010000000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/g/g,"0000001000000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/h/g,"0000000100000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/i/g,"0000000010000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/j/g,"0000000001000000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/k/g,"0000000000100000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/l/g,"0000000000010000000000000002");
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/m/g,"0000000000001000000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/n/g,"0000000000000100000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/o/g,"0000000000000010000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/p/g,"0000000000000001000000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/q/g,"0000000000000000100000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/r/g,"0000000000000000010000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/s/g,"0000000000000000001000000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/t/g,"0000000000000000000100000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/u/g,"0000000000000000000010000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/v/g,"0000000000000000000001000002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/w/g,"0000000000000000000000100002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/x/g,"0000000000000000000000010002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/y/g,"0000000000000000000000001002"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/z/g,"0000000000000000000000000102"); 
+
+/********************************* Encoding Upper Case
+*********************************/
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/A/g,"1000000000000000000000000012"); 
+
+document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/B/g,"0100000000000000000000000012"); 
+
+document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/C/g,"0010000000000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/D/g,"0001000000000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/E/g,"0000100000000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/F/g,"0000010000000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/G/g,"0000001000000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/H/g,"0000000100000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/I/g,"0000000010000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/J/g,"0000000001000000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/K/g,"0000000000100000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/L/g,"0000000000010000000000000012");
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/M/g,"0000000000001000000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/N/g,"0000000000000100000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/O/g,"0000000000000010000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/P/g,"0000000000000001000000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/Q/g,"0000000000000000100000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/R/g,"0000000000000000010000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/S/g,"0000000000000000001000000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/T/g,"0000000000000000000100000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/U/g,"0000000000000000000010000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/V/g,"0000000000000000000001000012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/W/g,"0000000000000000000000100012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/X/g,"0000000000000000000000010012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/Y/g,"0000000000000000000000001012"); 
+
+ document.querySelectorAll("[data-sy-01=true]")[i].innerHTML=document.querySelectorAll("[data-sy-01=true]")[i].innerHTML.replace(/Z/g,"0000000000000000000000000112"); 
+}
+
+var j;
+for (j=0;j<document.querySelectorAll("[data-sy-01-decode=true]").length;j++)
+{
+/*********************************  Decoding Rules For Lower Case
+*********************************/
+
+document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/1000000000000000000000000002/g,"a"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0100000000000000000000000002/g,"b"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0010000000000000000000000002/g,"c"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0001000000000000000000000002/g,"d"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000100000000000000000000002/g,"e"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000010000000000000000000002/g,"f"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000001000000000000000000002/g,"g"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000100000000000000000002/g,"h"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000010000000000000000002/g,"i"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000001000000000000000002/g,"j"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000100000000000000002/g,"k"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000010000000000000002/g,"l"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000001000000000000002/g,"m"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000100000000000002/g,"n"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000010000000000002/g,"o"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000001000000000002/g,"p"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000100000000002/g,"q"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000010000000002/g,"r"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000001000000002/g,"s"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000100000002/g,"t"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000010000002/g,"u"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000001000002/g,"v"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000100002/g,"w"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000010002/g,"x"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000001002/g,"y"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000000102/g,"z"); 
+
+/*********************************
+Decoding Rules For Upper Case
+*********************************/
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/1000000000000000000000000012/g,"A"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0100000000000000000000000012/g,"B"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0010000000000000000000000012/g,"C"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0001000000000000000000000012/g,"D"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000100000000000000000000012/g,"E"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000010000000000000000000012/g,"F"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000001000000000000000000012/g,"G"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000100000000000000000012/g,"H"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000010000000000000000012/g,"I"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000001000000000000000012/g,"J"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000100000000000000012/g,"K"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000010000000000000012/g,"L"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000001000000000000012/g,"M"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000100000000000012/g,"N"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000010000000000012/g,"O"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000001000000000012/g,"P"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000100000000012/g,"Q"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000010000000012/g,"R"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000001000000012/g,"S"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000100000012/g,"T"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000010000012/g,"U"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000001000012/g,"V"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000100012/g,"W"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000010012/g,"X"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000001012/g,"Y"); 
+
+ document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML=document.querySelectorAll("[data-sy-01-decode=true]")[j].innerHTML.replace(/0000000000000000000000000112/g,"Z"); 
+
+}
+
+
+
+/*
+var k;
+for (k=0;k<document.querySelectorAll("").length;k++)
+{
+
+}
+*/
+
+}
